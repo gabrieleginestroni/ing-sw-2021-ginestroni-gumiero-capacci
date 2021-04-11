93099a76c22c3cd78b262a6f0ec429c067144945
gabrieleginestroni <79751463+gabrieleginestroni@users.noreply.github.com>
2021-04-11 15:33:24 +0200

    Added documentation for cardslot and strongbox

--- src/main/java/it/polimi/ingsw/model/board/CardSlot.java
+++ src/main/java/it/polimi/ingsw/model/board/CardSlot.java
@@ -5,13 +5,29 @@ import it.polimi.ingsw.model.cards.DevelopmentCard;
 
 import java.util.ArrayList;
 
+/**
+ * @author Gabriele Ginestroni
+ * Class that represents a player's board's Development Card pile. The card slot can contain upto three development cards.
+ */
 public class CardSlot {
     private final ArrayList<DevelopmentCard> devCards;
 
+    /**
+     * Constructor of an empty card slot
+     */
     public CardSlot() {
         this.devCards = new ArrayList<>();
     }
 
+    /**
+     * Places a development card on top of the pile if the card's level is 1-level higher than the actual top card of
+     * pile.
+     * @param card Development card to add
+     * @throws developmentCardSlotLimitExceededException In case of placement of the fourth card
+     * @throws invalidDevelopmentCardLevelPlacementException In case of placements that does not meet the level requirement
+     *
+     *
+     */
     public void addCard(DevelopmentCard card) throws developmentCardSlotLimitExceededException,
             invalidDevelopmentCardLevelPlacementException {
 
@@ -31,18 +47,36 @@ public class CardSlot {
 
     }
 
+    /**
+     * Gets the top card of the pile
+     * @return Pile's top development card
+     */
     public DevelopmentCard getTopCard(){
         return this.devCards.get(0);
     }
 
+    /**
+     * Computes the pile's total number of Development Card that meet the parameters criteria
+     * @param level Level of Development Card
+     * @param color Color of Development Card
+     * @return Pile's total number of Development Card that meet the parameters criteria
+     */
     public int getCardNumber(int level, Color color){
         return this.devCards.stream().filter(c->c.getLevel()>=level && c.getType() == color).mapToInt(c->1).sum();
     }
 
+    /**
+     * Computes total number of victory points of the card pile
+     * @return Sum of all victory points of the pile's development cards
+     */
     public int getVictoryPoints(){
         return this.devCards.stream().mapToInt(DevelopmentCard::getVictoryPoints).sum();
     }
 
+    /**
+     * Computes the number of cards in the pile
+     * @return  number of Development cards in the pile
+     */
     public int getGenericCardNumber(){
         return devCards.size();
     }
--- src/main/java/it/polimi/ingsw/model/board/StrongBox.java
+++ src/main/java/it/polimi/ingsw/model/board/StrongBox.java
@@ -5,23 +5,45 @@ import it.polimi.ingsw.model.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
+/**@author Gabriele Ginestroni
+ * Class that represents the player's Strongbox. It can store all types of resources
+ */
 public class StrongBox {
     private final Map<Resource,Integer> resources;
 
+    /**
+     * Constructor of an empty Strongbox
+     */
     public StrongBox() {
         this.resources = new HashMap<>();
     }
 
+    /**
+     * Adds an amount of resource to the strongbox
+     * @param res Type of resource
+     * @param quantity Resource amount to add
+     */
     public void addResource(Resource res,int quantity){
         int newQuantity;
         newQuantity = resources.getOrDefault(res,0) + quantity;
         resources.put(res,newQuantity);
     }
 
+    /**
+     * Getter of the amount of resource contained in the strongbox
+     * @param res Type of resource
+     * @return Amount of resource contained in the strongbox
+     */
     public int getResource(Resource res){
         return resources.getOrDefault(res,0);
     }
 
+    /**
+     * Removes an amount of resource from the strongbox
+     * @param res Type of resource
+     * @param quantity Resource amount to remove
+     * @throws invalidStrongBoxRemoveException In case the quantity to remove is greater than the resource amount stored
+     */
     public void removeResource(Resource res,int quantity) throws invalidStrongBoxRemoveException {
         int newQuantity;
         newQuantity = resources.getOrDefault(res,0) - quantity;
@@ -31,6 +53,10 @@ public class StrongBox {
         resources.put(res,newQuantity);
     }
 
+    /**
+     * Computes the total amount of resource contained in the strongbox
+     * @return Total amount of resources contained in the strongbox
+     */
     public int getGenericResourceNumber(){
         int coinTot = getResource(Resource.COIN);
         int shieldTot = getResource(Resource.SHIELD);
