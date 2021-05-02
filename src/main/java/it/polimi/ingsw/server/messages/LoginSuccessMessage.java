92c57747ef87cf4fd67d005b0c43a64a82f92b3a
Tommaso Capacci <79759122+TommasoCapacci@users.noreply.github.com>
2021-05-02 22:42:10 +0200

    fixed typo

--- src/main/java/it/polimi/ingsw/server/messages/LoginSuccessMessage.java
+++ src/main/java/it/polimi/ingsw/server/messages/LoginSuccessMessage.java
@@ -8,11 +8,16 @@ public class LoginSuccessMessage extends AnswerMessage {
     private final String currentPlayers;
 
     public LoginSuccessMessage(List<Player> currentPlayers) {
-        this.currentPlayers = currentPlayers.toString();
+        StringBuilder str = new StringBuilder();
+
+        for(Player player : currentPlayers)
+            str.append(player.getNickname()).append("\n");
+
+        this.currentPlayers = str.toString();
     }
 
     @Override
     public void selectView() {
-        System.out.println("Login success. Current players: " + currentPlayers);
+        System.out.println("Login success. Current players: \n" + currentPlayers);
     }
 }
